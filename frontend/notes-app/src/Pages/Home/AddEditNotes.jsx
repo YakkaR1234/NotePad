eb4893fdc9010b00819c4baa5b106e1319f84426
YakkaR1234 <yashodpeiris2002@gmail.com>
2024-08-02 17:12:15 +0530

    Froonend_done

--- frontend/notes-app/src/Pages/Home/AddEditNotes.jsx
+++ frontend/notes-app/src/Pages/Home/AddEditNotes.jsx
@@ -1,38 +1,88 @@
+import { useState } from "react";
 import TagInput from "../../components/input/TagInput";
+import { MdClose } from "react-icons/md";
 
-const AddEditNotes = () => {
-    return (
-      <div>
-        <div className="flex flex-col gap-2">
-          <label className="input-label">TITLE</label>
-          <input 
-            type="text"
-            className="input-box text-2xl text-slate-950 outline-none"
-            placeholder="Go to the gym"
-          />
-        </div>
-        
-        <div className="flex flex-col gap-2 mt-4">
-          <label className="input-label">CONTENT</label>
-          <textarea 
-            type="text"
-            className="input-box text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
-            placeholder="Content"
-            rows={10}
-          />
-        </div>
-        
-        <div className="mt-3">
-          <label className="input-label">TAGS</label>
-          <TagInput/>
-        </div>
+const AddEditNotes = ({ noteData,type,onClose }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [tags, setTags] = useState([]);
+
+  const [error,setError]=useState(null);
+
+  const addNewNote=async ()=>{};
+  const editNote=async ()=>{};
+
+
+  const handleAddNote = () => {
   
-        <button className="btn-primary font-medium mt-5 p-3" onClick={() => {}}>
-          ADD
-        </button>
-      </div>
-    );
+    if(!title){
+      setError("please enter the title");
+      return;
+    }
+    if(!content){
+      setError("please enter the content");
+      return;
+    }
+
+    setError("");
+
+
+    if(type==='edit'){
+      editNote()
+    }else{
+      addNewNote()
+    }
   };
+
   
-  export default AddEditNotes;
-  
+
+  return (
+    <div className="relative">
+      <button
+        className="w-10 h-10 rounded-full flex items-center justify-center absolute -top-3 -right-3 hover:bg-slate-50"
+        onClick={onClose}
+      >
+        <MdClose className="text-xl text-slate-400" />
+      </button>
+
+      <div className="flex flex-col gap-2">
+        <label className="input-label">TITLE</label>
+        <input
+          type="text"
+          className="input-box text-2xl text-slate-950 outline-none"
+          placeholder="Go to the gym"
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+
+      <div className="flex flex-col gap-2 mt-4">
+        <label className="input-label">CONTENT</label>
+        <textarea
+          type="text"
+          className="input-box text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
+          placeholder="Content"
+          rows={10}
+          value={content}
+          onChange={({ target }) => setContent(target.value)}
+        />
+      </div>
+
+      <div className="mt-3">
+        <label className="input-label">TAGS</label>
+        <TagInput tags={tags} setTags={setTags} />
+      </div>
+
+      {error && <p className="text-red-500 text-xs pt-4">{error}</p>}
+
+      <button
+        className="btn-primary font-medium mt-5 p-3"
+        onClick={handleAddNote}
+      >
+        ADD
+      </button>
+    </div>
+  );
+};
+
+export default AddEditNotes;
--- frontend/notes-app/src/Pages/Home/Home.jsx
+++ frontend/notes-app/src/Pages/Home/Home.jsx
@@ -5,8 +5,6 @@ import AddEditNotes from "./AddEditNotes";
 import { useState } from "react";
 import Modal from "react-modal";
 
-
-
 const Home = () => {
   const [openAddEditModal, setOpenAddEditModal] = useState({
     isShown: false,
@@ -24,9 +22,9 @@ const Home = () => {
 
   return (
     <>
-      <Navbar /> 
+      <Navbar />
 
-      <div className="container mx-auto"> 
+      <div className="container mx-auto">
         <div className="grid grid-cols-3 gap-4 mt-8">
           <NoteCard
             title="Sample Note"
@@ -40,6 +38,7 @@ const Home = () => {
           />
         </div>
       </div>
+      
       <button
         className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
         onClick={handleOpenModal}
@@ -58,7 +57,11 @@ const Home = () => {
         className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
         contentLabel="Add or Edit Note"
       >
-        <AddEditNotes />
+        <AddEditNotes 
+          type={openAddEditModal.type}
+          noteData={openAddEditModal.data}
+          onClose={handleCloseModal}
+        />
       </Modal>
     </>
   );
