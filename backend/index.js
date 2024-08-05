4210d6bf18c7d7490927e4ca4d6278c7f7217cf9
YakkaR1234 <yashodpeiris2002@gmail.com>
2024-08-05 23:20:56 +0530

    login-bkend-done

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ const app=express();
 
 const jwt=require("jsonwebtoken");
 const {authenticationToken}=require("./Utilities");
+const e = require("express");
 
 app.use(express.json());
 
@@ -26,9 +27,7 @@ app.use(
     })
 );
 
-app.get("/",(req,res)=>{
-    res.json({data:"hello"});
-});
+
 
 //create account
 app.post("/create-account",async (req,res)=>{
@@ -64,6 +63,35 @@ app.post("/create-account",async (req,res)=>{
 
     return res.json({error:false,user,accessToken,message:"Registeration Successful"});
 
+
+});
+
+
+app.post("/login",async(req,res)=>{
+    const {email,password}=req.body;
+
+    if(!email){return res.status(400).json({message:"Email is requried"});}
+    if(!password){return res.status(400).json({message:"Password is required"});}
+
+    const userInfo=await User.findOne({email:email});
+
+    if(!userInfo){return res.status(400).json({message:"User not found"});}
+
+    if(userInfo.email==email&& userInfo.password==password){
+        const user={user:userInfo};
+        const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:"30000m"});
+
+        return res.json({
+            error:false,
+            message:"Login Successful",
+            email,
+            accessToken,
+        });
+    }else{
+        return res.status(400).json({error:true,message:"Invalid Credentials",})
+    }
+
+    
 
 });
 
